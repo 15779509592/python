ff2d3fb05a63d5b82267e8ff6ee756d3f0243529
15779509592 <43081824+15779509592@users.noreply.github.com>
2018-09-13 22:33:25 +0800

    Add files via upload

---
+++ 214-drawpython.py
@@ -0,0 +1,41 @@
+#DrawPython.py
+import turtle
+turtle.setup(650,350,200,200)
+turtle.penup()
+turtle.fd(-350)
+turtle.pendown()
+turtle.pensize(25)
+turtle.pencolor("gold")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+turtle.pencolor("red")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+turtle.pencolor("violet")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+turtle.pencolor("purple")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+turtle.pencolor("darkgreen")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+turtle.pencolor("grey")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+turtle.circle(40,80/2)
+turtle.fd(40)
+turtle.circle(16,180)
+turtle.fd(40*2/3)
